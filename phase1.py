362a1cde4aac21fe537fc7afef9b004531e5bf12
mohammedFazil3 <fazilm8777@gmail.com>
2024-11-10 18:48:39 +0300

    Added Playfair & Columnar - Fazil
    Test code is also included for encrypt and decrypt based on the d2l examples.

--- phase1.py
+++ phase1.py
@@ -84,3 +84,165 @@ def affineCipherDecrypt(cipherText,k,a):
 affineCipherDecrypt("ibwwv",5,2)
 
 
+#####Fazil - Playfair & Columnar
+
+#Playfair
+def generate_playfair_matrix(keyword):
+    keyword = keyword.upper().replace("J", "I")
+    seen = set()
+    matrix = []
+    
+    for char in keyword:
+        if char not in seen and char.isalpha():
+            seen.add(char)
+            matrix.append(char)
+
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    for char in alphabet:
+        if char not in seen:
+            matrix.append(char)
+
+    playfair_matrix = []
+    for i in range(5):
+        row = []
+        for j in range(5):
+            row.append(matrix[i * 5 + j])
+        playfair_matrix.append(row)
+    
+    return playfair_matrix
+
+def find_position(matrix, char):
+    for i in range(5):
+        for j in range(5):
+            if matrix[i][j] == char:
+                return i, j
+    return None
+
+def playfair_encrypt(plaintext, keyword):
+    matrix = generate_playfair_matrix(keyword)
+    plaintext = plaintext.upper().replace("J", "I").replace(" ", "")
+    encrypted_text = ""
+    
+    i = 0
+    while i < len(plaintext):
+        if i + 1 == len(plaintext) or plaintext[i] == plaintext[i + 1]:
+            pair = plaintext[i] + "X"
+            i += 1
+        else:
+            pair = plaintext[i] + plaintext[i + 1]
+            i += 2
+
+        row1, col1 = find_position(matrix, pair[0])
+        row2, col2 = find_position(matrix, pair[1])
+
+        if row1 == row2:
+            encrypted_text += matrix[row1][(col1 + 1) % 5] + matrix[row2][(col2 + 1) % 5]
+        elif col1 == col2:
+            encrypted_text += matrix[(row1 + 1) % 5][col1] + matrix[(row2 + 1) % 5][col2]
+        else:
+            encrypted_text += matrix[row1][col2] + matrix[row2][col1]
+
+    return encrypted_text
+
+def playfair_decrypt(ciphertext, keyword):
+    matrix = generate_playfair_matrix(keyword)
+    decrypted_text = ""
+    
+    i = 0
+    while i < len(ciphertext):
+        pair = ciphertext[i] + ciphertext[i + 1]
+        i += 2
+
+        row1, col1 = find_position(matrix, pair[0])
+        row2, col2 = find_position(matrix, pair[1])
+
+        if row1 == row2:
+            decrypted_text += matrix[row1][(col1 - 1) % 5] + matrix[row2][(col2 - 1) % 5]
+        elif col1 == col2:
+            decrypted_text += matrix[(row1 - 1) % 5][col1] + matrix[(row2 - 1) % 5][col2]
+        else:
+            decrypted_text += matrix[row1][col2] + matrix[row2][col1]
+
+    return decrypted_text
+
+#Test - Playfair Encryption
+print("PLAYFAIR TEST")
+cipher=playfair_encrypt("lord Granvilles letter","palmerstone")
+print(cipher)
+
+plaintext = playfair_decrypt(cipher,"palmerstone")
+print(plaintext)
+
+#####Fazil - Columnar Transposition
+import math
+
+def reOrderKey(t):
+    asciiConv=[]
+    for i in t:
+        asciiConv.append(ord(i))
+    ordered = sorted(asciiConv)
+    
+    l=[]
+    for i in asciiConv:
+        l.append(ordered.index(i))
+    
+    return l
+
+def columnarTransposition(m,k):
+    keyOrder = reOrderKey(k)
+    keyLength = len(k)
+    fillLetters = ['u','v','w','x','y','z']
+    i=0
+    m=m.split(" ")
+    
+    k=""
+    for i in m:
+        k+=i
+    m=k
+    
+    remainder = len(m) % keyLength
+    if remainder != 0:
+        padding_needed = keyLength - remainder
+        padding_index = 0
+        while padding_needed > 0:
+            m += fillLetters[padding_index % len(fillLetters)]
+            padding_index += 1
+            padding_needed -= 1
+    
+  
+    l=[]
+    for i in range(len(keyOrder)):
+        l.append(m[i::keyLength])
+        
+    
+    dictionary={}
+    index=0
+    for i in keyOrder:
+        dictionary[i] = l[index]
+        index+=1
+    
+    c=""
+    for key in (sorted(dictionary)):
+        c+=dictionary[key]
+
+    return c
+
+def decryptColumnarTransposition(c,k):
+    rows = len(c)/len(k)
+    cipherBlock=[]
+    block=""
+    i=0
+    
+    m=""
+    for i in range(int(rows)):
+        for j in reOrderKey(k):
+            m+=c[i+int(j*rows)]    
+    
+    return m
+
+#Test - Columnar
+print("Columnar Transposition Test")
+cipher=columnarTransposition("attackpostponeduntiltwoam","ciphers")
+print(cipher)
+plaintext=decryptColumnarTransposition(cipher,"ciphers")
+print(plaintext)
